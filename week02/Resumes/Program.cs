504bb9fb903480c2418de2a27275bc83c877191d
Opio Moses <catchproblems00@gmail.com>
2025-05-23 12:56:40 +0300

    Add files via upload

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Developer";
+        job1._company = "Tech Company";
+        job1._startYear = 2020;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Senior Software Developer";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Opio Moses";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
---
+++ week02/Resumes/PromptGenerator.cs
@@ -0,0 +1,8 @@
+public class PromptGenerator
+{
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+
+}
